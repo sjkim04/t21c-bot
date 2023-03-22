0a0a7aa5349f92de1aedeec51b2beaf78c86f7b8
sjk <sungntung@gmail.com>
2023-03-22 22:46:26 +0900

    chore: change bot invite link

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ module.exports = {
 		const embed = new EmbedBuilder()
 			.setColor(0x42268d)
 			.setTitle('Invite links!')
-			.setDescription('Bot invite link:\nhttps://discord.com/api/oauth2/authorize?client_id=1087228141300371578&permissions=139586702400&scope=bot\n\nT21+C Server:\nhttps://discord.gg/8FBDmAPrKe\n\nT21+C Management Sheet:\nhttps://docs.google.com/spreadsheets/d/1eaA1gyZ-6OWFthHFcVTfLV62U_MbpP6PHc8udN24iCg');
+			.setDescription('Bot invite link:\nhttps://discord.com/api/oauth2/authorize?client_id=1087228141300371578&permissions=8&scope=bot\n\nT21+C Server:\nhttps://discord.gg/8FBDmAPrKe\n\nT21+C Management Sheet:\nhttps://docs.google.com/spreadsheets/d/1eaA1gyZ-6OWFthHFcVTfLV62U_MbpP6PHc8udN24iCg');
 		interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 };
