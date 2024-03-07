26b81fe2027b2c9d2bfd6f9919fe61490f95e84c
sjk <sungntung@gmail.com>
2024-03-07 11:29:27 +0900

    chore(ready): remove unneeded await

--- events/ready.js
+++ events/ready.js
@@ -59,6 +59,6 @@ module.exports = {
 			console.error(error);
 		}
 
-		await console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
