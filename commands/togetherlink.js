9811f7852372339778cb5bbe7dee8271a0fe112f
sjk <sungntung@gmail.com>
2024-03-11 03:57:13 +0900

    feat: togetherlink

---
+++ commands/togetherlink.js
@@ -0,0 +1,50 @@
+const { default: axios } = require('axios');
+const { SlashCommandBuilder } = require('discord.js');
+const { togetherApiHost } = require('../config.json');
+// const { togetherApiHost, togetherEncKey } = require('../config.json');
+// const { encryptText } = require('../utils/general');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('togetherlink')
+		.setDescription('Link your Discord account to your Together mod account')
+		.addStringOption(option =>
+			option.setName('modusername')
+				.setDescription('the account name registered in the Together mod')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('linkcode')
+				.setDescription('link code sent to your email')
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+
+		const discordUsername = interaction.user.username;
+		const discordUserID = interaction.user.id;
+		const togetherUsername = interaction.options.getString('modusername', true);
+		// const togetherLinkCode = encryptText(interaction.options.getString('linkcode', true), togetherEncKey);
+		const togetherLinkCode = interaction.options.getString('linkcode', true);
+
+		// console.log({
+		// 	discordUsername,
+		// 	discordUserID,
+		// 	togetherUsername,
+		// 	togetherLinkCode,
+		// });
+
+		let response;
+
+		try {
+			response = await axios.post(`${togetherApiHost}/linkDiscordAccounts?discordUsername=${discordUsername}&discordUserID=${discordUserID}&togetherUsername=${togetherUsername}&togetherLinkCode=${togetherLinkCode}`);
+		}
+		catch (error) {
+			console.error(error);
+			return interaction.editReply('Something went wrong in the Together universe... Please try again later.');
+		}
+
+		return interaction.editReply(response.data);
+	},
+};
--- utils/general.js
+++ utils/general.js
@@ -3,3 +3,12 @@ module.exports.getRandomInt = (min, max) => {
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+module.exports.encryptText = (content, secretKey) => {
+	const data = Array.from(content);
+	const key = Array.from(secretKey);
+	for (let i = 0; i < data.length; i++) {
+		data[i] = String.fromCharCode(data[i].charCodeAt(0) ^ key[i % key.length].charCodeAt(0));
+	}
+	return data.join('');
+};
